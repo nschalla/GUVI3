a4cb1857c98e28e2aebcfb7f4f250ad1a87aa5d2
Narayanaswamy challa <36071265+nschalla@users.noreply.github.com>
2019-12-17 12:27:25 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+if(a>b):
+	print(a,"is greater")
+elif(a==b):
+	print("both are same")
+else:
+	print(b,"is greater") 
